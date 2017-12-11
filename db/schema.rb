6bdcc677d1142062eafcc2fd1a48aa242beb4702
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-11 14:09:42 -0700

    show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211210458) do
+ActiveRecord::Schema.define(version: 20171211210657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,20 @@ ActiveRecord::Schema.define(version: 20171211210458) do
     t.string "class_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "shows", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "quanity"
+    t.integer "floor_seating"
+    t.integer "general_admission"
+    t.integer "vip_table"
+    t.integer "standing_room"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shows_on_user_id"
   end
 
   create_table "store_items", force: :cascade do |t|
@@ -103,5 +117,6 @@ ActiveRecord::Schema.define(version: 20171211210458) do
   add_foreign_key "gift_cards", "users"
   add_foreign_key "instructed_classes", "lessons"
   add_foreign_key "instructed_classes", "teachers"
+  add_foreign_key "shows", "users"
   add_foreign_key "store_items", "users"
 end
