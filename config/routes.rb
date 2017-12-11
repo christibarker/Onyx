8378b4d276b3c9228e0c5c198faa5904c932920a
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-11 13:27:02 -0700

    devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # get 'home/index'
+  # devise_for :users
+  # # get 'home/index'
 
   resources :home
   root "home#index"
+
+   devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
