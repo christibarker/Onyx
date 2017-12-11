18d2e35cf209c3f18fdc5f3ee6d7c1971ca308f8
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-11 13:48:42 -0700

    lessons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211203448) do
+ActiveRecord::Schema.define(version: 20171211204703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20171211203448) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_gift_cards_on_user_id"
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.date "date"
+    t.string "class_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "store_items", force: :cascade do |t|
