bf3729e6a532d05757ec5fa1d3646af402758a75
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-12 13:46:12 -0700

    static pages

--- restful_routing.rb
+++ restful_routing.rb
@@ -1,4 +1,21 @@
                   Prefix Verb   URI Pattern                            Controller#Action
+                         GET    /pages/:page(.:format)                 pages#gno
+                         GET    /pages/:page(.:format)                 pages#prices
+        new_user_session GET    /users/sign_in(.:format)               devise/sessions#new
+            user_session POST   /users/sign_in(.:format)               devise/sessions#create
+    destroy_user_session DELETE /users/sign_out(.:format)              devise/sessions#destroy
+       new_user_password GET    /users/password/new(.:format)          devise/passwords#new
+      edit_user_password GET    /users/password/edit(.:format)         devise/passwords#edit
+           user_password PATCH  /users/password(.:format)              devise/passwords#update
+                         PUT    /users/password(.:format)              devise/passwords#update
+                         POST   /users/password(.:format)              devise/passwords#create
+cancel_user_registration GET    /users/cancel(.:format)                users/registrations#cancel
+   new_user_registration GET    /users/sign_up(.:format)               users/registrations#new
+  edit_user_registration GET    /users/edit(.:format)                  users/registrations#edit
+       user_registration PATCH  /users(.:format)                       users/registrations#update
+                         PUT    /users(.:format)                       users/registrations#update
+                         DELETE /users(.:format)                       users/registrations#destroy
+                         POST   /users(.:format)                       users/registrations#create
                    shows GET    /shows(.:format)                       shows#index
                          POST   /shows(.:format)                       shows#create
                 new_show GET    /shows/new(.:format)                   shows#new
@@ -64,18 +81,3 @@
                          PUT    /home/:id(.:format)                    home#update
                          DELETE /home/:id(.:format)                    home#destroy
                     root GET    /                                      home#index
-        new_user_session GET    /users/sign_in(.:format)               devise/sessions#new
-            user_session POST   /users/sign_in(.:format)               devise/sessions#create
-    destroy_user_session DELETE /users/sign_out(.:format)              devise/sessions#destroy
-       new_user_password GET    /users/password/new(.:format)          devise/passwords#new
-      edit_user_password GET    /users/password/edit(.:format)         devise/passwords#edit
-           user_password PATCH  /users/password(.:format)              devise/passwords#update
-                         PUT    /users/password(.:format)              devise/passwords#update
-                         POST   /users/password(.:format)              devise/passwords#create
-cancel_user_registration GET    /users/cancel(.:format)                users/registrations#cancel
-   new_user_registration GET    /users/sign_up(.:format)               users/registrations#new
-  edit_user_registration GET    /users/edit(.:format)                  users/registrations#edit
-       user_registration PATCH  /users(.:format)                       users/registrations#update
-                         PUT    /users(.:format)                       users/registrations#update
-                         DELETE /users(.:format)                       users/registrations#destroy
-                         POST   /users(.:format)                       users/registrations#create
