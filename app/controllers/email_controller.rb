bbef5cea9af15618431c91bf86a921433fc9a7ee
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-22 12:35:36 -0700

    pony update

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,14 +1,12 @@
 class EmailController < ApplicationController
 require 'pony'
   def index
-  	# Pony.mail(:to => 'you@example.com', :via => :smtp) # sends via SMTP
-  	# Pony.mail(:to => 'you@example.com', :from => 'me@example.com', :subject => 'hi', :body => 'Hello there.')
-  	Pony.mail(:to => 'kaco7777@gmail.com', :from => 'contact_params(:email)', :subject => 'contact_params(:subject)', :body => 'contact_params(:message)')
+  	Pony.mail(:to => 'kaco7777@gmail.com', :from => params[:email], :subject => params[:subject], :body => params[:message])
   end
 
-	private
+	# private
 
-  def contact_params
-    params.require(:contact).permit(:first_name, :last_name, :email, :subject, :message)
-  end
+ #  def contact_params
+ #    params.require(:contact).permit(:first_name, :last_name, :email, :subject, :message)
+ #  end
 end
