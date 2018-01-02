78147f2d32540d6fed7c2acd6f622b053cd4e1e4
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2018-01-01 17:02:29 -0700

    db update

--- db/migrate/20171212213254_add_fieldname_to_table_instructed_classes.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldnameToTableInstructedClasses < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :instructed_classes, :class_size, :integer  
-  end
-end
--- db/migrate/20171220222417_add_fieldname_to_table_instructed_class.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldnameToTableInstructedClass < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :instructed_classes, :class_size, :integer  
-  end
-end
