0c218c119dab4492771d95db9e03050f4bf6562e
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2018-01-07 11:15:26 -0700

    user update params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
-  access all: [:show, :index], user: {except: [:destroy, :create]}, admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :create, :update]}, admin: :all
 
   # GET /resource/sign_up
   def new
