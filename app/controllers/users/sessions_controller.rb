db57267aced5c6232b04b779fd1a970428f5f91d
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2018-01-07 11:11:56 -0700

    user params

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
+  access all: [:show, :index], user: {except: [:destroy, :create]}, admin: :all
 
   # GET /resource/sign_in
   def new
